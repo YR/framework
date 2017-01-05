26a0f53e93422d79a293dac5e41e8e4fa7e4de99
Alexander Pope <alex@pope-industries.com>
2017-01-05 15:42:32 +0100

    clean up

--- src/lib/pageHandlerFactory-server.js
+++ src/lib/pageHandlerFactory-server.js
@@ -1,6 +1,13 @@
 'use strict';
 
-const Page = require('./Page');
+const {
+  INITING,
+  INITED,
+  HANDLING,
+  HANDLED,
+  RENDERING,
+  RENDERED
+} = require('./Page');
 
 /**
  * Handler factory for 'page'
@@ -10,19 +17,19 @@ const Page = require('./Page');
 module.exports = function pageHandlerFactory (page) {
   return function pageHandler (req, res, next) {
     page.state = 0;
-    page.appendState(Page.INITING);
+    page.appendState(INITING);
     page.init((err) => {
-      page.appendState(-Page.INITING, Page.INITED);
+      page.appendState(-INITING, INITED);
       if (err) return next(err);
-      page.appendState(Page.HANDLING);
+      page.appendState(HANDLING);
       page.handle(req, res, (err) => {
-        page.appendState(-Page.HANDLING, Page.HANDLED);
+        page.appendState(-HANDLING, HANDLED);
         if (err) return next(err);
         // Prevent rendering unhandled/aborted
-        if (page.state === Page.INITED | Page.HANDLED) {
-          page.appendState(Page.RENDERING);
+        if (page.state === INITED | HANDLED) {
+          page.appendState(RENDERING);
           page.render(req, res, (err) => {
-            page.appendState(-Page.RENDERING, Page.RENDERED);
+            page.appendState(-RENDERING, RENDERED);
             if (err) return next(err);
             if (!res.finished) res.end();
           });
